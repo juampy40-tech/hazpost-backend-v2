a0577b974a20999abef4b88a25df2c6bd3e45f30
juampy40-tech <juampy40@gmail.com>
2026-04-23 18:10:37 -0500

    index.tsx

--- artifacts/social-dashboard/src/pages/index.tsx
+++ artifacts/social-dashboard/src/pages/index.tsx
@@ -1,8 +1,1 @@
-export default function Home() {
-  return (
-    <div style={{ padding: 40 }}>
-      <h1>🚀 HazPost funcionando</h1>
-      <p>Frontend conectado correctamente</p>
-    </div>
-  );
-}
+export { default } from "./landing";
